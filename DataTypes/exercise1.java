e097c228e1a9f5b4b6a77ea7136cddc631238769
RohanStalin <119678840+RohanStalin@users.noreply.github.com>
2022-12-03 12:20:49 +0530

    Add files via upload

---
+++ DataTypes/exercise1.java
@@ -0,0 +1,31 @@
+package basic;
+
+public class exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean ioT=true;
+		System.out.println(ioT);
+		byte range=127;
+		System.out.println(range);
+		short range1=-32765;
+		System.out.println(range1);
+		int range2=300000000;
+		System.out.println(range2);
+		long range3=3487358457349430L;
+		System.out.println(range3);
+		double number=99.9;
+		System.out.println(number);
+		float number1=99.8f;
+		System.out.println(number1);
+		char letter='\u0080';//Unicode value
+		System.out.println(letter);
+		char letter2=65;
+		System.out.println(letter2);
+		char letter3='A';
+		System.out.println(letter3);
+		
+
+	}
+
+}
